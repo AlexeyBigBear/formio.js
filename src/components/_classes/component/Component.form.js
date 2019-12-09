08830f2500f392c170e0976b63c9212b9bf3398c
AlexeyBigBear <alexey@bigbearsoftware.ca>
2019-12-09 09:19:46 -0800

    removed extra fields from form builder

--- src/components/_classes/component/Component.form.js
+++ src/components/_classes/component/Component.form.js
@@ -20,42 +20,6 @@ export default function(...extend) {
           key: 'display',
           weight: 0,
           components: ComponentEditDisplay
-        },
-        {
-          label: 'Data',
-          key: 'data',
-          weight: 10,
-          components: ComponentEditData
-        },
-        {
-          label: 'Validation',
-          key: 'validation',
-          weight: 20,
-          components: ComponentEditValidation
-        },
-        {
-          label: 'API',
-          key: 'api',
-          weight: 30,
-          components: ComponentEditAPI
-        },
-        {
-          label: 'Conditional',
-          key: 'conditional',
-          weight: 40,
-          components: ComponentEditConditional
-        },
-        {
-          label: 'Logic',
-          key: 'logic',
-          weight: 50,
-          components: ComponentEditLogic
-        },
-        {
-          label: 'Layout',
-          key: 'layout',
-          weight: 60,
-          components: ComponentEditLayout
         }
       ]
     }
